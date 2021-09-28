5a094febaf346da8c59c09ade55c5e68e0d16348
DESKTOP-HQTBQPM\Lennath <echofct37@gmail.com>
2021-09-28 18:46:40 +0900

    commit photon

--- Assets/Scripts/PhotonSystem.cs
+++ Assets/Scripts/PhotonSystem.cs
@@ -12,6 +12,12 @@ public class PhotonSystem : MonoBehaviourPunCallbacks
     //部屋名
     public string randomRoomName;
 
+    public bool IsRoomIn;
+    public bool SoloPlay;
+
+    public int inRoomPlayerCount;
+    public int RoomMaxPlayers;
+
     //シーン切り替えで破壊されないようにする
     private void Awake()
     {
@@ -20,8 +26,40 @@ public class PhotonSystem : MonoBehaviourPunCallbacks
 
     private void Start()
     {
+        IsRoomIn = false;
+    }
+
+
+    public void ConnectMasterServer() 
+    {
         // PhotonServerSettingsの設定内容を使ってマスターサーバーへ接続する
         PhotonNetwork.ConnectUsingSettings();
+    }
+
+
+    public void ReaveMasterServer()
+    {
+        // PhotonServerSettingsの設定内容を使ってマスターサーバーへ接続する
+        PhotonNetwork.Disconnect();
+    }
+
+
+
+
+    // ルームを生成
+    public void CreateRoom(){
+
+        //部屋名決定
+        randomRoomName = Guid.NewGuid().ToString("N").Substring(0, 6);
+        Debug.Log("RandomCreated");
+
+        // 作成するルームのルーム設定を行う
+        var roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 8;
+
+        // ルームに参加する（ルームが存在しなければ作成して参加する）
+        PhotonNetwork.JoinOrCreateRoom(randomRoomName, roomOptions, TypedLobby.Default);
+
     }
 
     // マスターサーバーへの接続が成功した時に呼ばれるコールバック
@@ -31,44 +69,71 @@ public class PhotonSystem : MonoBehaviourPunCallbacks
     }
 
 
-    // ルーム名を指定して参加
-    public void CreateRoom()
-    {
-        //部屋名決定
-        randomRoomName = Guid.NewGuid().ToString("N").Substring(0, 6);
-
-        // 作成するルームのルーム設定を行う
-        var roomOptions = new RoomOptions();
-        roomOptions.MaxPlayers = 8;
-
-        // "Room"という名前のルームに参加する（ルームが存在しなければ作成して参加する）
-        PhotonNetwork.JoinOrCreateRoom(randomRoomName, roomOptions, TypedLobby.Default);
-    }
-
-
+    //ランダムの部屋番号を返す
     public string randomRoomNameFunc() 
     { 
         return randomRoomName;
+    }
+
+    //ルームの最大参加人数を返す
+    public string RoomMaxPlayersFunc()
+    {
+        //Debug.Log(RoomInfo.MaxPlayers());　このように書いてみたがだめだった。
+        return null;
+    }
+
+    //ルームの現在の参加人数を返す
+    public string RoomPlayerCountFunc()
+    {
+        return null;
+    }
+
+    //部屋から退出
+    public void LeaveRoomFunc() 
+    {
+        PhotonNetwork.LeaveRoom();
+
     }
 
 
     // ルーム名を指定して参加
     public void JointIdentyRoom(string roomName)
     {
+
         PhotonNetwork.JoinRoom(roomName);
-    }  
-
-
-
-    // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
-    public override void OnJoinedRoom()
-    {
-        // ランダムな座標に自身のアバター（ネットワークオブジェクト）を生成する
-        //var position = new Vector3(Random.Range(-3f, 3f), Random.Range(3f, 4f),0.0f);
-        //PhotonNetwork.Instantiate("Playersample2", position, Quaternion.identity);
     }
 
 
+
+    //コールバック色々
+
+
+    // ルームへの参加が成功した時に呼ばれるコールバック
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("ルームへ参加しました");
+        Debug.Log(randomRoomName);
+        IsRoomIn = true;
+    }
+
+    // ルーム名を指定したルームへの参加が失敗した時に呼ばれるコールバック
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log($"ルームへの参加に失敗しました: {message}");
+        IsRoomIn = false;
+    }
+
+    // ルームから退出した時に呼ばれるコールバック
+    public override void OnLeftRoom()
+    {
+        Debug.Log("ルームから退出しました");
+        IsRoomIn = false;
+    }
+
+
+
+
+    //プレイヤースポーン
     public GameObject ActivePlayerSporn(string ActivePlayerName) 
     {
         var position = new Vector3(0.0f,3.7f, -3.24f);
